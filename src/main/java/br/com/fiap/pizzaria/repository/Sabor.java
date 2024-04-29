f3ba7a4bd4665defd0268512d61389c5e4fcf09a
jixatos <j.gody2004@gmail.com>
2024-04-29 18:27:08 -0300

    Correção mínima dos pastas das classes e nomes das UK

--- src/main/java/br/com/fiap/pizzaria/repository/Sabor.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.pizzaria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Sabor extends JpaRepository<Sabor, Long> {
-}
