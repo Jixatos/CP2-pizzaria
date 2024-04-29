e7c4fdb8497b13bd7a8627379c6c16fd7064a977
jixatos <j.gody2004@gmail.com>
2024-04-29 11:49:04 -0300

    Conclusão da entity Opcional

---
+++ src/main/java/br/com/fiap/pizzaria/entity/Opcional.java
@@ -0,0 +1,45 @@
+package br.com.fiap.pizzaria.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Data
+@Entity()
+@Table(name = "TBL_OPCIONAL",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_SABOR_OPCIONAL", columnNames = {"NM_OPCIONAL", "SABOR_OPCIONAL"})
+        }
+)
+public class Opcional {
+
+    @Id
+    @SequenceGenerator(name = "SQ_OPCIONAL", sequenceName = "SQ_OPCIONAL", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_OPCIONAL")
+    @Column(name = "ID_OPCIONAL")
+    private Long id;
+
+    @Column(name = "NM_OPCIONAL")
+    private String nome;
+
+    @Column(name = "VALUE_OPCIONAL")
+    private BigDecimal preco;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(
+            name = "SABOR",
+            referencedColumnName = "ID_SABOR",
+            foreignKey = @ForeignKey(
+                    name = "FK_SABOR_OPCIONAL"
+            )
+    )
+    private Sabor sabor;
+}
